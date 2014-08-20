15b894a188b06d3d91de00a7f14b51c50ce2e65c
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-08-20 13:27:02 +0800

    support bower package.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,4 +1,4 @@
-$(function(){   
+$(function(){
 
 
     $("#reg").on({
@@ -18,7 +18,7 @@ $(function(){
                 }
             });
         }
-    });  
+    });
 });
 
     function toggle(name) {
