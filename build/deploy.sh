4b886116b536357acb4ec88b7598013b31ab6516
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-08-28 15:43:59 +0800

    add deploy script
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ build/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e -x
+git --version
+upstream=$1
+: ${upstream:=origin}
+: ${REPO:=git@github.com:PHPConf-TW/website-2014}
+git fetch $upstream
+if [ `git rev-list HEAD...$upstream/master --count` -ne 0 ]; then
+echo "not deploying"
+  exit 1
+fi
+
+npm i
+
+# remove dist folder for gh-pages
+rm -rf dist
+
+# XXX: use --reference when not in shallow clone
+#git clone $REPO --reference . -b gh-pages _public
+git clone $REPO --depth 1 -b gh-pages dist
+# remove cache files
+rm -rf dist/assets/js
+rm -rf dist/assets/css
+
+REV=`git describe --always`
+BUILD=git-$REV ./node_modules/.bin/bower install && ./node_modules/.bin/gulp release --env production
+cd dist
+git fetch --depth 1 origin master:master
+git add -A .
+echo "regen for $REV" | git commit-tree `git write-tree` -p `git rev-parse HEAD` -p $REV | xargs git reset --hard
+git push origin gh-pages -f
