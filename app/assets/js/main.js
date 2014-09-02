71db7259b4022da5dfd1e8a1dc44a3b724503b27
Jace Ju <jaceju@gmail.com>
2014-09-02 18:40:19 +0800

    重整 JS 程式

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,35 +1,39 @@
-$(function(){
+(function(w, d) {
+    'use strict';
+
+    var $w = $(w);
+    var $d = $(d);
+
+    $(function() {
 
 
-    $("#reg").on({
-        click: function(e) {
-            e.preventDefault();
-            return window.open($(this).attr("href"), '_blank');
-        }
+        $('#reg').on({
+            click: function(e) {
+                e.preventDefault();
+                return window.open($(this).attr('href'), '_blank');
+            }
+        });
+        $('nav a').on({
+            click: function(e) {
+                e.preventDefault();
+                return $('body').stop().scrollTo($($(this).attr('href')), {
+                    duration: ($(d).width() > 500) ? 800 : 0,
+                    offset: {
+                        left: 0,
+                        top: -0.03 * $(w).height()
+                    }
+                });
+            }
+        });
     });
-    $("nav a").on({
-        click: function(e) {
-            e.preventDefault();
-            return $("body").stop().scrollTo($($(this).attr("href")), {
-                duration: ($(document).width() > 500) ? 800 : 0,
-                offset: {
-                    left: 0,
-                    top: -0.03 * $(window).height()
-                }
-            });
-        }
+
+    $(w).scroll(function(event) {
+        var scroll = $(w).scrollTop();
+        // Do something
+        if (scroll > 500)
+            $('#gotop').show();
+        else
+            $('#gotop').hide();
     });
-});
 
-function toggle(name) {
-    $('.' + name).toggle();
-}
-
-$(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-    // Do something
-    if (scroll > 500)
-        $('#gotop').show();
-    else
-        $('#gotop').hide();
-});
+})(window, document);
