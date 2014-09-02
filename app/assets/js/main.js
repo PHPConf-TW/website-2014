2f19aa1b6fb2566935ed269055681ab166e7566f
Jace Ju <jaceju@gmail.com>
2014-09-02 21:29:07 +0800

    修正捲動效果

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -4,30 +4,44 @@
     var $w = $(w);
     var $d = $(d);
     var $g = $('#gotop');
+    var $b = (window.opera) ?
+        (document.compatMode === 'CSS1Compat' ? $('html') : $('body')) :
+        $('html,body');
+    var $s = $('section');
+    var currentId = '#top';
 
     $(function() {
 
-        $('nav a').on({
-            click: function(e) {
-                e.preventDefault();
-                return $('body').stop().scrollTo($($(this).attr('href')), {
-                    duration: 800,
-                    offset: {
-                        left: 0,
-                        top: '180px'
-                    }
-                });
+        $('nav a, #gotop a').on('click', function(e) {
+            e.preventDefault();
+
+            var $this = $(this);
+            var pageId = $this.attr('href');
+            var top = $(pageId).offset().top;
+
+            if (currentId === pageId) {
+                return false;
             }
+
+            $b.animate({
+                scrollTop: top
+            }, 600, function () {
+                window.location.hash = pageId;
+                $b.scrollTop(top);
+            });
+
+            currentId = pageId;
         });
     });
 
     $w.scroll(function(event) {
-        var scroll = $(w).scrollTop();
+        var scroll = $w.scrollTop();
         // Do something
-        if (scroll > 500)
+        if (scroll > 500) {
             $g.show();
-        else
+        } else {
             $g.hide();
+        }
     });
 
 })(window, document);
